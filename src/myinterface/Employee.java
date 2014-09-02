23048c7aa41579b5415ae831f18d71f4da6bf721
Josh Conley <>
2014-09-01 20:40:32 -0500

    Fourth commit; Progress on interface package

---
+++ src/myinterface/Employee.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myinterface;
+
+/**
+ *
+ * @author Josh
+ */
+public interface Employee {
+    
+    public abstract double calculateBiWeeklyPay ();
+    
+    // Getters and setters    
+    public abstract void setFirstName(String firstName);
+    public abstract String getFirstName();
+    public abstract String getLastName();
+    public abstract void setLastName(String lastName);
+    public abstract int getEmployeeId();
+    public abstract void setEmployeeId(int employeeId);
+    public abstract String getDepartment();
+    public abstract void setDepartment(String department);
+    public abstract double getPayRate();
+    public abstract void setPayRate(double payRate);
+    public abstract int getHoursWorked();
+    public abstract void setHoursWorked(int hoursWorked);
+}
---
+++ src/myinterface/HourlyEmployee.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myinterface;
+
+/**
+ *
+ * @author Josh
+ */
+public class HourlyEmployee implements Employee {
+    
+    private final int FORTY_HOURS = 40;
+    private String firstName;
+    private String lastName;
+    private int employeeId;
+    private String department;
+
+    private double payRate;
+    private int hoursWorked;
+
+    public HourlyEmployee(String firstName, String lastName, int employeeId, String department, double payRate, int hoursWorked) {
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double calculateBiWeeklyPay () {
+        double amountEarned;
+        double overTimeHours;
+        
+        if (hoursWorked <= FORTY_HOURS) {
+            amountEarned = payRate * hoursWorked;
+        }
+        else {
+            overTimeHours = hoursWorked - 40;
+            amountEarned = ((payRate * FORTY_HOURS) + ((payRate * 1.5) * overTimeHours));            
+        }
+        
+        return amountEarned;            
+    }
+
+    @Override
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    @Override
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    
+    @Override
+    public double getPayRate() {
+        return payRate;
+    }
+    
+    @Override
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+    
+    @Override
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    @Override
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }    
+}
